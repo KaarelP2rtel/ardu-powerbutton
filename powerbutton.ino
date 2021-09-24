2971b0074ec3b43d4ab63757c73828433f908f3d
Kaarel Pärtel <kaarelp2rtel@gmail.com>
2021-09-24 19:01:48 +0300

    Add HDD LED feedback
    The arduino can now intercept the HDD LED to also
    insert its own feedback into the LED.

--- powerbutton.ino
+++ powerbutton.ino
@@ -1,13 +1,21 @@
 #include <Bounce2.h>
 
-Bounce2::Button button = Bounce2::Button();
+// Configuration
+int powerPin = 3;
+int resetPin = 4;
+int ledPin = 8;
+int hddInput = 7;
+bool hddActive = LOW;
 
+// State
 int count = 0;
 unsigned int buttonLastReleased = 0;
 unsigned int buttonLastPressed = 0;
-int powerPin = 3;
-int resetPin = 4;
-int ledPin = 13;
+bool hddPaused = true;
+
+Bounce2::Button button = Bounce2::Button();
+Bounce2::Button hdd = Bounce2::Button();
+
 
 void setup()
 {
@@ -15,9 +23,14 @@ void setup()
     button.interval(10);
     button.setPressedState(LOW);
 
+    hdd.attach(hddInput);
+    hdd.interval(0);
+    hdd.setPressedState(hddActive);
+
     pinMode(powerPin, OUTPUT);
     pinMode(resetPin, OUTPUT);
     pinMode(ledPin, OUTPUT);
+    pinMode(hddInput, INPUT);
 
     digitalWrite(powerPin, HIGH);
     digitalWrite(resetPin, HIGH);
@@ -29,14 +42,32 @@ void setup()
 void loop()
 {
     button.update();
+    hdd.update();
+
+    if (hdd.duration() < 1000)
+    {
+        hddPaused = false;
+        digitalWrite(ledPin, hdd.isPressed());
+    }
+    else if (!hddPaused){
+        digitalWrite(ledPin, LOW);
+        hddPaused = true;
+    }
+
     if (button.released())
     {
         count++;
+        digitalWrite(ledPin, LOW);
         buttonLastReleased = millis();
     }
     if (button.pressed())
     {
+        digitalWrite(ledPin, HIGH);
         buttonLastPressed = millis();
+    }
+
+    if (button.isPressed()){
+        digitalWrite(ledPin, HIGH);
     }
 
     if (count == 3 && millisSince(buttonLastReleased) > 500)
@@ -66,6 +97,7 @@ void loop()
     {
         count = 0;
     }
+
 }
 
 int millisSince(unsigned int timestamp)
@@ -74,15 +106,15 @@ int millisSince(unsigned int timestamp)
 }
 
 void pressPowerPin()
-{   
-    
+{
+
     digitalWrite(powerPin, LOW);
     digitalWrite(ledPin, HIGH);
     Serial.println("Power pin pressed");
 }
 void releasePowerPin()
 {
-    
+
     digitalWrite(powerPin, HIGH);
     digitalWrite(ledPin, LOW);
     Serial.println("Power pin released");
